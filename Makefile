c0762e6bf6c82393877e4579f22b9ad5eb6a31da
ubuntu <hjkja0511@gmail.com>
2024-03-21 11:57:20 +0000

    test1

---
+++ Makefile
@@ -0,0 +1,4 @@
+add:
+	g++ npshell.cpp -o npshell
+clean: 
+	rm npshell
--- npshell.cpp
+++ npshell.cpp
@@ -3,6 +3,7 @@
 #include<sstream>
 #include<unistd.h>
 #include<map>
+#include<signal.h>
 #include<vector>
 #include<queue>
 #include<sys/wait.h>
@@ -32,18 +33,25 @@ tokenType
 /*
 operationType
 0 : init
-1 : write >
-2 : pipe |
-3 : pipenumber |1
-4 : exclamationnumber ?1
+1 : >
+2 : |
+3 ; !
+4 : |1
+5 : ?1
 */
+/*void signal_child(int signal){
+    int status;
+    wait(&status);
+    return;
+}*/
 
 struct command
 {
     command() : state(0), numberpipeleft(0), currentToken(""),
-                commandType(0), operationType(0) {}
+                commandType(0), previosOP(0), nextOP(0) {}
     int commandType;
-    int operationType;
+    int previosOP;
+    int nextOP;
     string currentToken;
     vector<string> tokenArgument;
     int state;
@@ -51,7 +59,15 @@ struct command
     vector<string> tokens;
 };
 
-queue<command> cmdqueue;
+struct pipestruct
+{
+    pipestruct() : numberleft(0), pipetype(0) {}
+    int numberleft; 
+    int pipetype; // 1 : | , 2 : ! , 3 : |1 , 4 : !1
+    int fd[2];
+};
+
+vector<pipestruct> pipes;
 map<string, string> envs;
 
 bool isBuildinCmd(command currentcmd){
@@ -75,56 +91,82 @@ void pipe(){
 
 }
 
-void processexec(int type){
-    
-    switch (type)
-    {
-    case 1:
-        
-        break;
-    case 2:
-        break;
-    case 3:
-        break;
-    case 4:
-        break;
-    case 5:
-        break;
-    case 6:
-        break;
-    default:
-        break;
-    }
-}
-
 void forkandexec(command &cmd){
 
+    
     int pid = fork();
     if(pid == 0) { // chld process
+        
+        //cout << "PreviosOp:" << cmd.previosOP << endl;
+        //cout << "NextOp:" << cmd.nextOP << endl;
+        switch (cmd.nextOP)
+        {
+        case 1:
+
+            if(cmd.previosOP != 0) dup2(pipes[pipes.size()-1].fd[0], STDIN_FILENO);
+            if(cmd.nextOP != 0) dup2(pipes[pipes.size()-1].fd[1], STDOUT_FILENO);
+            break;
+        default:
+            if(cmd.previosOP != 0) dup2(pipes[pipes.size()-1].fd[0], STDIN_FILENO);
+            break;
+        }
+        close(pipes[pipes.size()-1].fd[0]);
+        close(pipes[pipes.size()-1].fd[1]);
         int a = 0;
         if(cmd.tokenArgument.data() == 0){
+            //cout << "case1" << endl;
             a = execlp(cmd.currentToken.c_str(), cmd.currentToken.c_str(), 
                 NULL, NULL);
         } else {
+            //cout << "case2" << endl;
             a = execlp(cmd.currentToken.c_str(), cmd.currentToken.c_str(), 
                cmd.tokenArgument.data()->c_str(), NULL);  
         }
-        if(a==-1) cout << "error exec" << endl;
-        cout << " from child process" << pid  << endl;
+        cout << "11" << endl;
+
+        if(a==-1) cerr << "Undefined command" << endl;
+        exit(0);
     } else { // parent process
-        wait(NULL);
-        cout << " from parent process" << pid  << endl;
+        if(cmd.nextOP == 0){
+            close(pipes[pipes.size()-1].fd[0]);
+            close(pipes[pipes.size()-1].fd[1]);
+        }
+        waitpid(-1, NULL, WNOHANG);
+        /*if(cmd.nextOP == 0){
+            while(waitpid(-1, NULL, WNOHANG)==0){
+                //cout << 1 << endl;
+                //sleep(1);
+            }
+        } else {
+            waitpid(pid, NULL, 0);
+        }*/
+        
     }
-    //execlp(cmd.currentToken.c_str(), cmd.tokenArgument.data()->c_str());
 }
 
 void processToken(command &cmd){
     int i = 0;
+
     for(;i<cmd.tokens.size();++i) {
         if(cmd.currentToken == "") cmd.currentToken = cmd.tokens[i];
-        else cmd.tokenArgument.push_back(cmd.tokens[i]);
+        else if(cmd.tokens[i][0] != '|') cmd.tokenArgument.push_back(cmd.tokens[i]);
         
-        if(cmd.tokens[i][0] == '|' || i == cmd.tokens.size()-1) forkandexec(cmd);
+        if(cmd.tokens[i][0] == '|' || i == cmd.tokens.size()-1){
+            cmd.previosOP = cmd.nextOP;
+            if(i==cmd.tokens.size() - 1) cmd.nextOP = 0;
+            else {
+                cmd.nextOP = 1;
+                pipes.push_back(pipestruct{});
+                if(pipe(pipes[pipes.size()-1].fd) < 0) {
+                    cerr << "create pipe fail" << endl;
+                } 
+            }
+            
+            forkandexec(cmd);
+            cmd.currentToken = "";
+            cmd.tokenArgument.clear();
+        } 
+        
     }
 }
 
@@ -134,13 +176,13 @@ void processCommand(command &cmd){
         if(cmd.tokens[0] == "setenv"){
 
             if(cmd.tokens.size() < 3){
-                cout << "loss parameters" << endl;
+                cerr << "loss parameters" << endl;
                 return;
             } 
             setenv(cmd.tokens[1].c_str(), cmd.tokens[2].c_str(), 1);
         } else if(cmd.tokens[0] == "printenv"){
             if(cmd.tokens.size()<2){
-                cout << "loss parameters" << endl;
+                cerr << "loss parameters" << endl;
                 return;
             } 
 
@@ -158,7 +200,6 @@ void executable(){
 
     string cmdLine;
     stringstream ss;
-    setenv("PATH" , "bin:.", 1);
     while(cout << "% " && getline(cin, cmdLine)){
         command currentcmd;
         
@@ -187,6 +228,9 @@ void executable(){
 }
 
 int main(){
+    //void signal_child(int);
+    //signal(SIGCHLD, signal_child);
+    setenv("PATH" , "bin:.", 1);
     executable();
     
     return 0;
